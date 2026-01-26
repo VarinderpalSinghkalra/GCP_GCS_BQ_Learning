3668bed1df957179edf61d0e364d882f457b835d
Varinder pal Singh <52190718+VarinderpalSinghkalra@users.noreply.github.com>
2026-01-26 17:34:46 +0530

    Create config.py

---
+++ Conversational chatbot/working-example/DocumentAI_usecase/config.py
@@ -0,0 +1,10 @@
+# Document AI
+PROJECT_NUMBER = "245950264824"
+LOCATION = "us"
+PROCESSOR_ID = "2c3a091983206ae3"
+
+# BigQuery
+BQ_PROJECT_ID = "my-big-query-project-479504"
+BQ_DATASET = "document_insights"
+BQ_TABLE = "w9_extracted_data"
+
