e510fdb88883f99eaa88c8bf858f9ccc1e3fb45a
Varinder pal Singh <52190718+VarinderpalSinghkalra@users.noreply.github.com>
2026-04-19 10:05:01 +0530

    Update destroy.sh

--- azure-terraform-assignment/destroy.sh
+++ azure-terraform-assignment/destroy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Destroying VMSS..."
 cd vmss
 terraform destroy -auto-approve
 
-echo "Destroying Storage..."
 cd ../storage
 terraform destroy -auto-approve
-
-echo "🧹 Cleanup Done"
