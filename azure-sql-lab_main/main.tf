e9475bd100a13c0357de133eb4934f699059345a
Varinder pal Singh <52190718+VarinderpalSinghkalra@users.noreply.github.com>
2026-06-14 11:14:14 +0530

    Update main.tf

--- azure-sql-lab_main/main.tf
+++ azure-sql-lab_main/main.tf
@@ -1,22 +1,12 @@
-###################################
-# Resource Group
-###################################
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-###################################
-# SQL SERVER
-###################################
-
 resource "azurerm_mssql_server" "sqlserver" {
-
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
-
   version                      = "12.0"
 
   administrator_login          = var.sql_admin_user
@@ -25,16 +15,108 @@ resource "azurerm_mssql_server" "sqlserver" {
   minimum_tls_version = "1.2"
 }
 
-###################################
-# SQL DATABASE
-###################################
-
 resource "azurerm_mssql_database" "sqldb" {
-
   name      = "SampleDB"
   server_id = azurerm_mssql_server.sqlserver.id
 
   sku_name = "Basic"
+}
 
-  zone_redundant = false
+resource "azurerm_mssql_firewall_rule" "myip" {
+  name             = "AllowMyIP"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "YOUR_PUBLIC_IP"
+  end_ip_address   = "YOUR_PUBLIC_IP"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "gl-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "gl-vm-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "gl-vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+
+    source_port_range          = "*"
+    destination_port_range     = "22"
+
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "gl-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "GL-SQL-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  size = "Standard_B1s"
+
+  admin_username = var.vm_admin_username
+  admin_password = var.vm_admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 }
